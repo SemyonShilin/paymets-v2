596baefadd23a6c3157810fe6852dfce1162d169
ShilinSemyon <shilin2048@gmail.com>
2016-04-29 07:35:55 +0300

    changing paymetns _form

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -4,7 +4,7 @@ class Payment < ActiveRecord::Base
   validates :payment, presence: true
   validates :payment, numericality: {greater_than_or_equal_to: 0.01,
                                      message: 'Сумма платеж не может быть отрицательным'}
-  validates :other_payment, numericality: {greater_than_or_equal_to: 0.01,
+  validates :other_payment, numericality: {greater_than_or_equal_to: 0,
                                            message: 'Сумма платеж не может быть отрицательным'},
             unless: Proc.new {|a| a.other_payment.blank? }
   #validates :description, uniqueness: true, unless: Proc.new {|a| a.description.blank? }
